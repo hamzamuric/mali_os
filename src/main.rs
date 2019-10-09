1247bae768da21a6ffa780575e3230260e4b4682
Hamza Muric <hamzamuric9@gmail.com>
2019-10-09 18:39:53 +0200

    double fault handler

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,16 @@ pub extern "C" fn _start() -> ! {
 
     mali_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    fn stack_overflow() {
+        stack_overflow();
+    }
+
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
 
-    println!("It did not crash!");
+    println!("Radi!");
     loop {}
 }
 
